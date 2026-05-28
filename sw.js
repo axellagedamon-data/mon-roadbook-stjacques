c622601e92d540478d6d06a2c43246f4fcb16bfb
LAGEDAMON <axel.lagedamon@cgi.com>
2026-05-28 09:38:24 +0200

    Initial commit - Roadbook St Jacques

---
+++ sw.js
@@ -0,0 +1,44 @@
+// Service Worker pour l'assistance du Trail de St Jacques
+const CACHE_NAME = 'st-jacques-assistance-v1';
+const ASSETS = [
+  './',
+  './roadbook_trail_de_st_jacques.html',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request).then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    }).catch(() => {
+      return caches.match('./roadbook_trail_de_st_jacques.html');
+    })
+  );
+});
